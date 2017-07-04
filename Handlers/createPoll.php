107d3094ddb2361c454518c4e2483f2ad9c5f297
jedi <niko.metreveli@gmail.com>
2017-07-04 23:04:37 +0400

    added handler to create poll

---
+++ Handlers/createPoll.php
@@ -0,0 +1,16 @@
+<?php
+  require_once('../core/autoLoadClass.php');
+  require_once('../Database/dbHelper.php');
+  if(!empty($_POST['question']) && !empty($_POST['answer'])){
+  		$question = $_POST['question'];
+  		$answers = $_POST['answer'];
+		$dbHelp = new dbHelp;
+		$insertedQuestionId = $dbHelp->insert("Question",array("question" => $question));
+		$link = md5($insertedQuestionId."1039");
+		$dbHelp->insert("Link",array("link" => $link,"questionID" => $insertedQuestionId));
+		foreach ($answers as $item) {
+			if(!empty($item))
+				$dbHelp->insert("Answers",array("answer"=>$item,"questionID"=>$insertedQuestionId));
+		}
+  }
+?>
--- index.php
+++ index.php
@@ -2,22 +2,23 @@
 <?php
 require_once './Includes/Partials/header.php';
 ?>
+<form action="Handlers/createPoll.php" method="POST" >
 <fieldset>
-  <legend><input type="text" name="" class="question" placeholder="Type your Question here" style="" spellcheck="false"></legend>
+  <legend><input type="text" name="question" class="question" placeholder="Type your Question here" style="" spellcheck="false"></legend>
 
-    <input type="text" name="quality" id="accessible" class="answer" placeholder="Type your answer here">
+    <input type="text" name="answer[]" id="accessible" class="answer" placeholder="Type your answer here">
 
-    <input type="text" name="quality" id="accessible" class="answer" placeholder="Type your answer here">
+    <input type="text" name="answer[]" id="accessible" class="answer" placeholder="Type your answer here">
 
-    <input type="text" name="quality" id="accessible" class="answer" placeholder="Type your answer here">
+    <input type="text" name="answer[]" id="accessible" class="answer" placeholder="Type your answer here">
 
-    <input type="text" name="quality" id="accessible" class="answer" placeholder="Type your answer here">
+    <input type="text" name="answer[]" id="accessible" class="answer" placeholder="Type your answer here">
 
-    <input type="text" name="quality" id="accessible" class="answer" placeholder="Type your answer here">
+    <input type="text" name="answer[]" id="accessible" class="answer" placeholder="Type your answer here">
 
 </fieldset>
 <input type="submit" name="submit" class="voteButton" value="Create Poll">
-
+</form>
 <?php
 	require_once './Includes/Partials/footer.php';
 ?>
