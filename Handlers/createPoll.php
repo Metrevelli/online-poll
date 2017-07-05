73b6c871d2706bbb6996878614275d86796a30d8
jedi <niko.metreveli@gmail.com>
2017-07-05 22:58:26 +0400

    finished createpoll handler now it inserts question,answers and link

--- Handlers/createPoll.php
+++ Handlers/createPoll.php
@@ -1,16 +1,17 @@
 <?php
-  require_once('../core/autoLoadClass.php');
-  require_once('../Database/dbHelper.php');
+  require_once '../core/autoLoadClass.php';
+  require_once '../Database/dbHelper.php';
   if(!empty($_POST['question']) && !empty($_POST['answer'])){
   		$question = $_POST['question'];
   		$answers = $_POST['answer'];
 		$dbHelp = new dbHelp;
 		$insertedQuestionId = $dbHelp->insert("Question",array("question" => $question));
-		$link = md5($insertedQuestionId."1039");
+		$link = $insertedQuestionId, 20, 36;
 		$dbHelp->insert("Link",array("link" => $link,"questionID" => $insertedQuestionId));
 		foreach ($answers as $item) {
 			if(!empty($item))
 				$dbHelp->insert("Answers",array("answer"=>$item,"questionID"=>$insertedQuestionId));
 		}
+
   }
 ?>
