ac9748124a17606e2c168ac4ff92eea3e5da25d5
jedi <niko.metreveli@gmail.com>
2017-07-13 23:33:35 +0400

    added functions to restrict user to vote twice on the same poll

--- func/functions.php
+++ func/functions.php
@@ -20,20 +20,51 @@ function createPoll($post){
 function generateLinks($get){
 	$dbHelp = new dbHelp;
 	$link = $dbHelp->select("link","Link",array("link"=>$get));
-	// print_r($link);
 	if(!empty($get) && !empty($link)){
 		return $link[0]["link"];
 	}
 	redirect::to("../index.php");
 }
 function insertUserVote($postt){
-	if(!empty($postt)){
+	$convertedLink = base_convert($postt["questionID"],10,36);
+	$realLink = $postt["questionID"];
+	$answerID = $postt['userAnswerID'];
+	if(!checkUserIpAddress($realLink)){
+		if(empty($answerID))
+			redirect::to("../vote.php?poll=$convertedLink");
 		$dbHelp = new dbHelp;
-		$answerID = $postt['userAnswerID'];
-		$link = base_convert($postt["questionID"],10,36);
 		$votes = $dbHelp->select("votes","Answers",array("answerID" => $answerID));
+		$insertIp = insertUserIpAddress($realLink);
 		$updateVotes = $dbHelp->update("Answers",array("votes" => ($votes[0]["votes"] + 1)),array("answerID" => $answerID));
-		if($updateVotes)
-			redirect::to("../result.php?result=$link");
+		if($updateVotes && $insertIp)
+			redirect::to("../result.php?result=$convertedLink");
 	}
+	redirect::to("../result.php?result=$convertedLink");
+}
+function insertUserIpAddress($questionID){
+	$dbHelp = new dbHelp;
+	$ip = getRealIpAddr();
+	$insertIp = $dbHelp->insert("userVotedPolls",array("IP_ADDRESS"=>$ip,"questionID"=>$questionID));
+	return $insertIp;
+}
+function checkUserIpAddress($questionID){
+	$dbHelp = new dbHelp;
+	$ip = getRealIpAddr();
+	$checkUserIp = $dbHelp->select("*","userVotedPolls",array("IP_ADDRESS"=>$ip,"questionID"=>$questionID));
+	return $checkUserIp;
+}
+function getRealIpAddr(){
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
 }
